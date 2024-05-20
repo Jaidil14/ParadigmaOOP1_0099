194691a0fabfbc58e6c09e5e09baf5a703bd2f5b
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-20 14:40:42 +0700

    Menambahkan class Mahasiswa

--- memberinitlist.cpp
+++ memberinitlist.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    Mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    {
+        // Member Initialization List
+        // Definisi
+    }
+
+    ~Mahasiswa() 
+    {
+        cout << "Id    = " << id << endl;
+        cout << "Nama  = " << id << endl;
+        cout << "Nilai = " << id << endl;
+    }
+};
+
