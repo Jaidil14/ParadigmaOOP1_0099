d0ff2b5ebae8d11044855b477a26d85c4d963fde
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-20 15:26:41 +0700

    Menambahkan fungsi main

--- Agregasi/mainIA.cpp
+++ Agregasi/mainIA.cpp
@@ -2,3 +2,27 @@
 #include <string>
 using namespace std;
 
+#include "anak.h"
+#include "ibu.h"
+
+int main()
+{
+    Ibu* varIbu = new Ibu("dini");
+    Ibu* varIbu2 = new Ibu("novi");
+    Anak* varAnak1 = new Anak("tono");
+    Anak* varAnak2 = new Anak("rini");
+    Anak* varAnak3 = new Anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+}
